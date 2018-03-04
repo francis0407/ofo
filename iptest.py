a8297640993afda7a47fc36475059a2c3af5adb4
francis0407 <hanmingcong123@hotmail.com>
2018-03-04 19:36:23 +0800

    IP test module

--- iptest.py
+++ iptest.py
@@ -3,7 +3,7 @@ import threading
 
 def error_handler():
     import time
-    time_string = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+    time_string = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     print("ErrorTime:%s"%(time_string))
     return
 
@@ -23,12 +23,43 @@ def test(i):
                 cur_time = time.time()
     return
 
+def iptest(threadid):
+    while 1:
+        try_time = time.time()
+        lock.acquire()
+        global start_time
+        if time.time() - start_time>= 600:
+            print("Running.. %s"%(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+            start_time = time.time()
+        lock.release()
+        while 1:
+            try:
+                info = spider.spider_single(121.332,31.297,0)
+                if info['code']!=200:
+                    print(info)
+                    print("线程%d退出"%(threadid))
+                    error_handler()
+                    return
+                break
+            except:
+                curtime = time.time()
+                if curtime - try_time <= 120:
+                    continue
+                else:
+                    print("线程%d请求超时，退出"%(threadid))
+                    error_handler()
+                    return
+
+
+
 import time
-time_string = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+time_string = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 print("StartTime:%s"%(time_string))
 thread = []
+lock = threading.Lock()
+start_time = time.time()
 for i in range(500):
-    thread.append(threading.Thread(target=test,args=(i,)))
+    thread.append(threading.Thread(target=iptest,args=(i,)))
 for i in range(500):
     thread[i].start()
 for i in range(500):
